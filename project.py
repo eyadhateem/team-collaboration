a2c9a2661f12a122611f6650249e3e5e687b2a53
eyadhateem <eyadhateem0@gmail.com>
2025-05-04 10:42:13 +0300

    Add files via upload

---
+++ project.py
@@ -0,0 +1,10 @@
+#ABOUTGAME
+print("WELCOM TO OUR TEXT_ADVENTURE_GAME")
+print("'GET OUT FROM school'")
+print()
+#STARTING
+x = input("click 0 to start")
+while x!="0":
+ x = input("please click 0")
+else:
+ print("you are a student , you find yourself standing in your class alone , the vibes is very scary and dark ")
